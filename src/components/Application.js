2b816f3a79118108263867f16e551eeed0b59856
GC <gc@GCs-MacBook-Pro.local>
2019-10-01 15:18:06 -0400

    added selector to application function to find the appointments for a specified day

--- src/components/Application.js
+++ src/components/Application.js
@@ -3,53 +3,54 @@ import DayList from "./DayList";
 import Appointment from "components/Appointment";
 import "components/Application.scss";
 import axios from "axios";
+import { getAppointmentsForDay } from "../helpers/selectors.js";
 
-const appointments = [
-  {
-    id: 1,
-    time: "12pm"
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm"
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Cohen",
-      interviewer: {
-        id: 1,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-    interview: {
-      student: "Hello-World",
-      interviewer: {
-        id: 2,
-        name: "Jason Chou",
-        avatar: "https://i.imgur.com/LpaY82x.png"
-      }
-    }
-  }
-];
+// const appointments = [
+//   {
+//     id: 1,
+//     time: "12pm"
+//   },
+//   {
+//     id: 2,
+//     time: "1pm",
+//     interview: {
+//       student: "Lydia Miller-Jones",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 3,
+//     time: "2pm"
+//   },
+//   {
+//     id: 4,
+//     time: "3pm",
+//     interview: {
+//       student: "Archie Cohen",
+//       interviewer: {
+//         id: 1,
+//         name: "Sylvia Palmer",
+//         avatar: "https://i.imgur.com/LpaY82x.png"
+//       }
+//     }
+//   },
+//   {
+//     id: 5,
+//     time: "4pm",
+//     interview: {
+//       student: "Hello-World",
+//       interviewer: {
+//         id: 2,
+//         name: "Jason Chou",
+//         avatar: "https://i.imgur.com/LpaY82x.png"
+//       }
+//     }
+//   }
+// ];
 
 export default function Application(props) {
   // const [day, setDay] = useState("Monday");
@@ -63,16 +64,26 @@ export default function Application(props) {
 
   const setDay = day => setState({ ...state, day });
   // const setDays = days => setState({ ...state, days });
-  const setDays = days => setState(prev => ({ ...prev, days }));
+  // const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
-    axios
-      .get("/api/days")
-      .then(res => {
-        setDays(res.data);
-      })
-      .catch(error => console.log(error));
+    Promise.all([axios.get("/api/days"), axios.get("/api/appointments")]).then(
+      all => {
+        const [days, appointments] = all;
+        // console.log(days, appointments);
+        setState(prev => ({ days: all[0].data, appointments: all[1].data }));
+      }
+    );
+
+    // axios
+    //   .get("/api/days")
+    //   .then(res => {
+    //     // setDays(res.data);
+    //   })
+    //   .catch(error => console.log(error));
   }, []);
+
+  const appointmentSchedule = getAppointmentsForDay(state, state.day);
 
   return (
     <main className="layout">
@@ -93,7 +104,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {appointments.map(appointmentTime => {
+        {appointmentSchedule.map(appointmentTime => {
           return (
             <Fragment key={appointmentTime.id}>
               <Appointment key={appointmentTime.id} {...appointmentTime} />
