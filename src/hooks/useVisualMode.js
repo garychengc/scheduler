97c47432538ab48bb1cbc81d89945d2e548ed553
GC <gc@GCs-MacBook-Pro.local>
2019-10-01 17:41:54 -0400

    added useVisualMode test case for back, transition, history array

---
+++ src/hooks/useVisualMode.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+export default function useVisualMode(initial) {
+  const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
+
+  function transition(newMode, replace = false) {
+    if (!replace) {
+      history.push(newMode);
+    }
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length === 1) {
+      setMode(history[0]);
+    } else {
+      history.pop();
+      setMode(history[history.length - 1]);
+    }
+  }
+
+  return { mode, transition, back };
+}
