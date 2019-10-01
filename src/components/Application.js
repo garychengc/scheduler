762ed8b14b39b855def5e4ab041ce0ce55339c46
GC <gc@GCs-MacBook-Pro.local>
2019-10-01 13:31:58 -0400

    added useState for both day, days, and appointments, change setState function in order to allow setDays to be used inside useEffect

--- src/components/Application.js
+++ src/components/Application.js
@@ -52,8 +52,18 @@ const appointments = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  // const [day, setDay] = useState("Monday");
+  // const [days, setDays] = useState([]);
+
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+
+  const setDay = day => setState({ ...state, day });
+  // const setDays = days => setState({ ...state, days });
+  const setDays = days => setState(prev => ({ ...prev, days }));
 
   useEffect(() => {
     axios
@@ -74,7 +84,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
