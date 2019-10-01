f778fde1c6b1ba52d77e991273e541d2c493e148
GC <gc@GCs-MacBook-Pro.local>
2019-10-01 12:50:15 -0400

    added selectors for getting the appointments with a specific day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,14 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(dayData => dayData.name === day);
+  console.log(filteredDay);
+  let filteredAppoinment = [];
+
+  if (filteredDay.length !== 0) {
+    for (let key in state.appointments) {
+      if (filteredDay[0].appointments.includes(state.appointments[key].id)) {
+        filteredAppoinment.push(state.appointments[key]);
+      }
+    }
+  }
+  return filteredAppoinment;
+}
